9df9f72ebd1f455d71e36f7e0e43a5365753c811
Nilesh Kumar <nilesh.kumar@deliverysolutions.co>
2024-05-14 23:35:35 +0530

    sequelize setup for db communication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfig } from './db/sequelize.config';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forRoot(SequelizeConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
