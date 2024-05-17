e13798531fb8bdd2005858696083c89fc9ddf26d
Nilesh Kumar <nilesh.kumar@deliverysolutions.co>
2024-05-18 03:45:12 +0530

    server run successful

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './configs/import-configuration';
+import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
 
+    console.log(join(__dirname, configService.getOrThrow('APP.PROTO_PATH')));
     const microserviceOpts: MicroserviceOptions = {
         transport: Transport.GRPC,
         options: {
-            package: configService.getOrThrow('PROTO_PACKAGE'),
-            protoPath: configService.getOrThrow('PROTO_PATH'),
-            url: configService.getOrThrow('MS_URL'),
+            package: configService.getOrThrow('APP.PROTO_PACKAGE'),
+            protoPath: join(
+                __dirname,
+                configService.getOrThrow('APP.PROTO_PATH'),
+            ),
+            url: configService.getOrThrow('APP.MS_URL'),
         },
     };
-    app.connectMicroservice(microserviceOpts);
+    app.connectMicroservice(microserviceOpts).listen();
 }
 
 bootstrap();
