172b016e3c880124a348828073e6e8cf2765b12c
Nilesh Kumar <nilesh.kumar@deliverysolutions.co>
2024-06-03 00:56:57 +0530

    handling duplicate entry error appropriately

--- src/exceptions/typeorm-sql/DuplicateEntry.exception.ts
+++ src/exceptions/typeorm-sql/DuplicateEntry.exception.ts
@@ -1,11 +1,16 @@
 import { status } from '@grpc/grpc-js';
 import { BaseGrpcException } from '../BaseGrpcException.factory';
 import { ERROR_CODE_LOOKUP, ERROR_NAMES } from './error-codes';
+import { QueryFailedError } from 'typeorm';
 
 export class DuplicateEntryException extends BaseGrpcException {
-    constructor(error: string | object) {
+    constructor(error: QueryFailedError) {
         const errorCode = status.ALREADY_EXISTS;
         const errorName = ERROR_CODE_LOOKUP[ERROR_NAMES.ER_DUP_ENTRY];
-        super(errorName, 'Duplicate entry', errorCode);
+
+        const regex = /'([^']*)'/g;
+        const fields = [...error.message.matchAll(regex)];
+
+        super(errorName, `duplicate value ${fields[0][0]}`, errorCode);
     }
 }
