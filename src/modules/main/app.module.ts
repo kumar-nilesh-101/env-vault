acf6a37e65bebd0fb1dcc791e7cee3e1a80efc06
Nilesh Kumar <nilesh.kumar@deliverysolutions.co>
2024-06-03 16:25:24 +0530

    environment module init

--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -5,11 +5,13 @@ import { ConfigsModule } from '../../configs/configs.module';
 import { DatabaseModule } from '../../db/database.module';
 import { ServiceRegistryModule } from '../service-registry/service-registry.module';
 import { VariableRegistryModule } from '../variable-registry/variable-registry.module';
+import { EnvironmentsModule } from '../environments/environments.module';
 
 @Module({
     imports: [
         ConfigsModule,
         DatabaseModule,
+        EnvironmentsModule,
         ServiceRegistryModule,
         VariableRegistryModule,
     ],
